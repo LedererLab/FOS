7fe5391fda4a94618db4418943c91f9acadf9140
Benjamin J Phillips <bephillips2@gmail.com>
2017-12-12 13:52:07 -0800

    Removed old, unused implementation of Coordinate Descent

--- Solvers/CoordinateDescent/coordinatedescentwithscreen.cpp
+++
@@ -1,6 +0,0 @@
-//Explicit Instantiation of CDSR for Python Wrappers
-
-#include "coordinatedescentwithscreen.hpp"
-
-template class hdim::CoordinateDescentWithScreen<float>;
-template class hdim::CoordinateDescentWithScreen<double>;
